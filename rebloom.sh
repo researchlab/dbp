3bcb06be401cc7924c161c6a5773385d749c9e8a
lihong <lihongitchina@gmail.com>
2018-10-26 10:04:19 +0800

    single redis by docker

--- rebloom.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -itd --name redisbloomfilter -p7001:6379 -v ~/workbench/docker/redis/conf/redis-rebloom.conf:/etc/redis/redis.conf redislabs/rebloom redis-server /etc/redis/redis.conf
--- redis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -itd --name lredis -p7002:6379 -v ~/workbench/docker/redis/conf/redis.conf:/etc/redis/redis.conf redis redis-server /etc/redis/redis.conf
