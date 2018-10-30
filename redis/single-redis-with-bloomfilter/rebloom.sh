89541ec2ffb7bf53a14374654b1590f262617e8b
lihong <lihongitchina@gmail.com>
2018-10-30 17:30:00 +0800

    update config path

--- redis/single-redis-with-bloomfilter/rebloom.sh
+++ redis/single-redis-with-bloomfilter/rebloom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -itd --name redisbloomfilter -p7001:6379 -v ~/workbench/docker/redis/single-redis-with-bloomfilter/conf/redis-rebloom.conf:/etc/redis/redis.conf redislabs/rebloom redis-server /etc/redis/redis.conf
+docker run -itd --name redisbloomfilter -p7001:6379 -v ~/workbench/docker/docker-envs/redis/single-redis-with-bloomfilter/conf/redis-rebloom.conf:/etc/redis/redis.conf redislabs/rebloom redis-server /etc/redis/redis.conf
--- redis/single-redis/redis.sh
+++ redis/single-redis/redis.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-docker run -itd --name lredis -p7002:6379 -v ~/workbench/docker/redis/single-redis/conf/redis.conf:/etc/redis/redis.conf redis redis-server /etc/redis/redis.conf
+docker run -itd --name lredis -p7002:6379 -v ~/workbench/docker/docker-envs/redis/single-redis/conf/redis.conf:/etc/redis/redis.conf redis redis-server /etc/redis/redis.conf
