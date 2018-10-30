4db55662a5f3c5e9c05ce95200356c1c0c7436ef
lihong <lihongitchina@gmail.com>
2018-10-30 19:20:43 +0800

    update mysql.sh

--- mysql/single-mysql/mysql.sh
+++ mysql/single-mysql/mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker pull mysql/mysql-server:5.7
+#docker pull mysql/mysql-server
 docker run --name=dev-mysql -d -p 3307:3306 -v ~/workbench/docker/docker-envs/mysql/sqls:/sqls/ -e MYSQL_USER=dev -e MYSQL_PASSWORD=dev123 -e MYSQL_ROOT_PASSWORD=dev123456 -e MYSQL_DATABASE=testdb  mysql/mysql-server
