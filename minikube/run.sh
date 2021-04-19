7cbbd41b7b108e6abe9d637b263949e0857de411
hong li <lihongitchina@gmail.com>
2021-04-19 18:26:29 +0800

    update

--- minikube/run.sh
+++ minikube/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
+# 1087  vpn port
+localip=$(ifconfig |grep 'netmask' | grep 10.21.71 | cut -d' ' -f2)
 minikube delete && minikube start --cpus=4 --memory=4096 --disk-size=10g \
-	--docker-env http_proxy=10.21.71.152:1087 \
-	--docker-env https_proxy=10.21.71.152:1087 \
+	--docker-env http_proxy=${localip}:1087 \
+	--docker-env https_proxy=${localip}:1087 \
 	--docker-env no_proxy=localhost,127.0.0.1,10.96.0.0/12,192.168.99.0/24,192.168.39.0/24 \
 	--docker-env no_proxy=$no_proxy,$(minikube ip) \
 	--registry-mirror=https://registry.docker-cn.com \
