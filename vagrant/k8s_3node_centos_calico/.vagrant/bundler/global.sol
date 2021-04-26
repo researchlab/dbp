c4baa68fe1cd885eb96b35320ff7e57d28d94671
hong li <lihongitchina@gmail.com>
2021-04-26 11:26:55 +0800

    clean .vagrant

--- vagrant/k8s_3node_centos_calico/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"0073ae91ba64ddefa487e3f786497ba1f42dce167176cc869c78e72f74d95b45","vagrant_version":"2.2.15"}
