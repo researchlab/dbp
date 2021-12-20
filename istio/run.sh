556189725d81e9591d929d2d53125f47792cebf3
hong li <lihongitchina@gmail.com>
2021-12-20 21:28:20 +0800

    update istio

--- istio/run.sh
+++ istio/run.sh
@@ -1,11 +1,2 @@
-#!/bin/bash 
+helm install install/kubernetes/helm/istio --name istio --namespace istio-system --set tracing.enabled=true --set kiali.enabled=true --set grafana.enabled=true
 
-export http_proxy=http://192.168.1.101:1087
-export https_proxy=http://192.168.1.101:1087
-export NO_PROXY=192.168.99.112
-
-minikube start --docker-env HTTP_PROXY=$http_proxy --docker-env HTTPS_PROXY=$https_proxy
-
-# 启动集群后，还要再特别忽略集群节点的代理
-export no_proxy=$no_proxy,$(minikube ip)
-export NO_PROXY=$no_proxy,$(minikube ip)
